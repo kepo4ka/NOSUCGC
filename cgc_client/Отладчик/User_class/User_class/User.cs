a8bcbfaa43136571d78c28566579cc4dd13e034c
kepo4ka <konstantin.bugulov@mail.ru>
2018-05-05 22:35:14 +0300

    UPDATE

--- cgc_client/Отладчик/User_class/User_class/User.cs
+++ cgc_client/Отладчик/User_class/User_class/User.cs
@@ -9,40 +9,83 @@ namespace User_class
     [Serializable]
     public class User : Player
     {
-        public static GameBoard gameboard;
+        public GameBoard gameboard;
+
+         static int lastX;
+         static int lastY;
+        static PlayerAction lastAction;
+
+
+
+
 
 
         /// <summary>
         /// Задать Команду на следующий Ход
         /// </summary>
-        /// <param name="gb">Полученное Игровое Поле</param>
-        /// <returns>Команда для бота</returns>
+        /// <param name="gb">Игровое Поле</param>
+        /// <returns>Команда для Юнита</returns>
         public override PlayerAction Play(GameBoard gb)
         {
             /*
                 Код пользователя
             */
-         
             Random rn = new Random();
             gameboard = gb;
 
+            Console.WriteLine(gameboard.Bonuses.Count);
+
             PlayerAction action = PlayerAction.Wait;
 
-            if ((gb.W / 2 - X) > 0)
+            int difX = (X - gb.W / 2);
+            int difY = (Y - gb.H / 2);
+
+
+            if (lastAction == PlayerAction.Right && gb.XYinfo[X + 1, Y].Free != true)
+            {
+               return action = PlayerAction.Bomb;
+            }
+            if (lastAction == PlayerAction.Bomb && gb.XYinfo[X + 1, Y].Free != true)
+            {
+               return action = PlayerAction.Left;
+            }
+            if (lastAction == PlayerAction.Left)
+            {
+               return action = PlayerAction.Left;
+            }
+
+
+            if (gb.XYinfo[lastX, lastY].Free != true)
+            {
+                action = PlayerAction.Bomb;
+            }
+            else if (difY > 0)
+                action = PlayerAction.Up;
+            else if (difY < 0)
+            {
+                action = PlayerAction.Down;
+            }
+
+            else if (difX > 0)
+            {
+                action = PlayerAction.Left;
+            }
+            else if (difX < 0)
             {
                 action = PlayerAction.Right;
-                return action;
             }
-            else 
+
+            lastX = X;
+            lastY = Y;
+            lastAction = action;
+
+            /*
+             Код пользователя
+           */
 
 
             return action;
         }
-
-
-
-
-
 
     }
 }
